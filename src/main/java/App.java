f09ad15cc097ff8697b32585fbd653bae253c796
Joe Kramer <joekramer1125@gmail.com>
2017-07-03 15:08:32 -0700

    save task to session and display on index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,9 +11,20 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("task", request.session().attribute("task"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-    
+
+    post("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String description = request.queryParams("description");
+      Task newTask = new Task(description);
+      request.session().attribute("task", newTask);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
