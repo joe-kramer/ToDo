3f6a2524d2fe8393b31497dd5605d9acd4cabd35
Joe Kramer <joekramer1125@gmail.com>
2017-07-10 08:37:38 -0700

    add db file and create connection to test database in test file, run clear table after every test

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,14 +1,23 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
+import org.sql2o.*;
 
 public class TaskTest {
 
   @Before
-  public void tearDown() {
-    Task.clear();
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
   }
 
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM tasks *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+  
   @Test
   public void Task_instantiatesCorrectly_true() {
     Task myTask = new Task("Mow the lawn");
