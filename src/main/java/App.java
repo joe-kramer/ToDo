7837e3509795f070255ff1b7f6df4624af710681
Joe Kramer <joekramer1125@gmail.com>
2017-07-10 14:44:49 -0700

    on post of new description, call update in this method. then redirect using category and task id

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -30,19 +30,19 @@ public class App {
       Category category = Category.find(Integer.parseInt(request.params(":category_id")));
       Task task = Task.find(Integer.parseInt(request.params(":id")));
       model.put("category", category);
-      model.put("tasks", task;
-      model.put("template", "templates/tasks.vtl");
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/tasks", (request, response) -> {
+    post("/categories/:category_id/tasks/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
+      Task task = Task.find(Integer.parseInt(request.params("id")));
       String description = request.queryParams("description");
-      Task newTask = new Task(description, category.getId());
-      newTask.save();
-      model.put("category", category);
-      model.put("template", "templates/category-tasks-success.vtl");
+      Category category = Category.find(task.getCategoryId());
+      task.update(description);
+      String url = String.format("/categories/%d/tasks/%d", category.getId(), task.getId());
+      response.redirect(url);
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
