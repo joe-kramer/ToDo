c2575c112077adad632e126915d2a2b8c16f94ac
Joe Kramer <joekramer1125@gmail.com>
2017-07-11 09:22:27 -0700

    add delete button to task page

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -78,7 +78,7 @@ public class Task {
 
   public void delete() {
     try(Connection con = DB.sql2o.open();) {
-      String sql "DELETE FROM tasks WHERE id = :id;";
+      String sql = "DELETE FROM tasks WHERE id = :id;";
       con.createQuery(sql)
       .addParameter("id", this.id)
       .executeUpdate();
